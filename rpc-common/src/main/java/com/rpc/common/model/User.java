c0746da6f85322498656b448c149fa159662ec9c
Hangz <1506620085@qq.com>
2025-06-17 15:07:48 +0800

    添加User实体与服务

---
+++ rpc-common/src/main/java/com/rpc/common/model/User.java
@@ -0,0 +1,15 @@
+package com.rpc.common.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 用户
+ */
+@Data
+public class User implements Serializable {
+
+    private String name;
+
+}
