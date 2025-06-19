4e1cb7d05c2f19cba20aa4e651f7670319adc6e8
Hangz <1506620085@qq.com>
2025-06-19 17:20:05 +0800

    RPC请求与响应对象添加

--- rpc-provider/src/main/java/com/rpc/provider/SimpleProviderExample.java
+++ rpc-provider/src/main/java/com/rpc/provider/SimpleProviderExample.java
@@ -1,12 +1,16 @@
 package com.rpc.provider;
 
+import com.rpc.common.service.UserService;
+import com.rpc.demo.registry.LocalRegistry;
 import com.rpc.demo.server.HttpServer;
 import com.rpc.demo.server.VertxHttpServer;
-import lombok.extern.slf4j.Slf4j;
+import com.rpc.provider.service.UserServiceImpl;
 
 
 public class SimpleProviderExample {
     public static void main(String[] args) {
+        // 注册服务
+        LocalRegistry.register(UserService.class.getName(), UserServiceImpl.class);
         // 启动 web 服务
         HttpServer httpServer = new VertxHttpServer();
         httpServer.doStart(8080);
