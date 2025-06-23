082c31f0fa65583fd55327522cfbc1be924a6686
Hangz <1506620085@qq.com>
2025-06-23 18:17:16 +0800

    动态代理

--- rpc-consumer/src/main/java/com/rpc/consumer/SimpleConsumerExample.java
+++ rpc-consumer/src/main/java/com/rpc/consumer/SimpleConsumerExample.java
@@ -2,14 +2,18 @@ package com.rpc.consumer;
 
 import com.rpc.common.model.User;
 import com.rpc.common.service.UserService;
+import com.rpc.demo.proxy.ServiceProxyFactory;
+
 
 /**
  * 简易服务消费者示例
  */
-public class SimpleConsumer {
+public class SimpleConsumerExample {
     public static void main(String[] args) {
         // 静态代理
-        UserService userService = new UserServiceProxy();
+//        UserService userService = new UserServiceProxy();
+        // 动态代理
+        UserService userService = ServiceProxyFactory.getProxy(UserService.class);
         User user = new User();
         user.setName("Hangz");
         // 调用
---
+++ rpc-simple-demo/src/main/java/com/rpc/demo/proxy/ServiceProxy.java
@@ -0,0 +1,52 @@
+package com.rpc.demo.proxy;
+
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
+import com.rpc.demo.model.RpcRequest;
+import com.rpc.demo.model.RpcResponse;
+import com.rpc.demo.serializer.JdkSerializer;
+import com.rpc.demo.serializer.Serializer;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+
+/**
+ * 动态代理
+ */
+public class ServiceProxy implements InvocationHandler {
+
+    /**
+     * 调用代理
+     *
+     * @return
+     * @throws Throwable
+     */
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        // 指定序列化器
+        Serializer serializer = new JdkSerializer();
+        // 构造请求
+        RpcRequest rpcRequest = RpcRequest.builder()
+                .serviceName(method.getDeclaringClass().getName())
+                .methodName(method.getName())
+                .parameterTypes(method.getParameterTypes())
+                .args(args)
+                .build();
+        try {
+            // 序列化
+            byte[] bodyBytes = serializer.serialize(rpcRequest);
+            // 发送请求
+            byte[] result;
+            try (HttpResponse httpResponse = HttpRequest.post("http://localhost:8080")
+                    .body(bodyBytes)
+                    .execute()) {
+                result = httpResponse.bodyBytes();
+            }
+            RpcResponse deserialize = serializer.deserialize(result, RpcResponse.class);
+            return deserialize.getData();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
