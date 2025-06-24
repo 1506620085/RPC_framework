7d3b3c8b3ba76ab2a723cea2234795d2f6d484d5
Hangz <1506620085@qq.com>
2025-06-24 18:10:12 +0800

    添加配置类

---
+++ rpc-core/src/main/java/com/rpc/demo/config/RpcConfig.java
@@ -0,0 +1,39 @@
+package com.rpc.demo.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+/**
+ * RPC 框架配置
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class RpcConfig implements Serializable {
+
+    /**
+     * 名称
+     */
+    private String name = "Hangz";
+
+    /**
+     * 版本号
+     */
+    private String version = "1.0";
+
+    /**
+     * 服务器主机名
+     */
+    private String serverHost = "localhost";
+
+    /**
+     * 服务器端口号
+     */
+    private Integer serverPort = 8080;
+
+}
---
+++ rpc-core/src/main/java/com/rpc/demo/utils/ConfigUtils.java
@@ -0,0 +1,7 @@
+package com.rpc.demo.utils;
+
+/**
+ * 配置工具类
+ */
+public class ConfigUtils {
+}
