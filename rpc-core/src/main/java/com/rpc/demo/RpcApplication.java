5aa68cb0f31e09f9ad0bce43e70bdce9a5cf6bdc
Hangz <1506620085@qq.com>
2025-06-25 16:56:09 +0800

    全局配置加载

---
+++ rpc-core/src/main/java/com/rpc/demo/RpcApplication.java
@@ -0,0 +1,48 @@
+package com.rpc.demo;
+
+import com.rpc.demo.config.RpcConfig;
+import com.rpc.demo.constant.RpcConstant;
+import com.rpc.demo.utils.ConfigUtils;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * RPC 框架应用
+ * 相当于 holder，存放了项目全局用到的变量。双检锁单例模式实现
+ */
+@Slf4j
+public class RpcApplication {
+
+    private static volatile RpcConfig rpcConfig;
+
+    public static void init(RpcConfig newRpcConfig) {
+        rpcConfig = newRpcConfig;
+        log.info("rpc init, config = {}", newRpcConfig.toString());
+    }
+
+    public static void init() {
+        RpcConfig newRpcConfig;
+        try {
+            newRpcConfig = ConfigUtils.loadConfig(RpcConfig.class, RpcConstant.DEFAULT_CONFIG_PREFIX);
+        } catch (Exception e) {
+            // 配置加载失败，使用默认值
+            newRpcConfig = new RpcConfig();
+        }
+        init(newRpcConfig);
+    }
+
+    /**
+     * 获取配置
+     *
+     * @return
+     */
+    public static RpcConfig getRpcConfig() {
+        if (rpcConfig == null) {
+            synchronized (RpcApplication.class) {
+                if (rpcConfig == null) {
+                    init();
+                }
+            }
+        }
+        return rpcConfig;
+    }
+}
