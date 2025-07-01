ccedd3e24a18f8ff979599fd1c9104cb36791f11
Hangz <1506620085@qq.com>
2025-07-01 17:51:56 +0800

    编写SPI加载器

---
+++ rpc-core/src/main/java/com/rpc/core/spi/SpiLoader.java
@@ -0,0 +1,71 @@
+package com.rpc.core.spi;
+
+import com.rpc.core.serializer.Serializer;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * SPI 加载器（支持键值对映射）
+ */
+@Slf4j
+public class SpiLoader {
+
+    /**
+     * 存储已加载的类：接口名 => （key => 实现类）
+     */
+    public static Map<String, Map<String, Class<?>>> loaderMap = new ConcurrentHashMap<>();
+
+    /**
+     * 对象实例缓存（避免重复 new），类路径 => 对象实例，单例模式
+     */
+    private static Map<String, Object> instanceCache = new ConcurrentHashMap<>();
+
+    /**
+     * 系统 SPI 目录
+     */
+    private static final String RPC_SYSTEM_SPI_DIR = "META-INF/rpc/system/";
+
+    /**
+     * 用户自定义 SPI 目录
+     */
+    private static final String RPC_CUSTOM_SPI_DIR = "META-INF/rpc/custom/";
+
+    /**
+     * 扫描路径
+     */
+    private static final String[] SCAN_DIRS = new String[]{RPC_SYSTEM_SPI_DIR, RPC_CUSTOM_SPI_DIR};
+
+    /**
+     * 动态加载的类列表
+     */
+    private static final List<Class<?>> LOAD_CLASS_LIST = Arrays.asList(Serializer.class);
+
+    /**
+     * 加载所有类型
+     */
+    public static void loadAll() {
+        log.info("加载所有 SPI");
+        for (Class<?> aClass : LOAD_CLASS_LIST) {
+            load(aClass);
+        }
+    }
+
+    /**
+     * 获取某个接口的实例
+     *
+     * @param tClass
+     * @param key
+     * @param <T>
+     * @return
+     */
+    public static <T> T getInstance(Class<?> tClass, String key) {
+        String tClassName = tClass.getName();
+        Map<String, Class<?>> stringClassMap = loaderMap.get(key);
+    }
+
+
+}
