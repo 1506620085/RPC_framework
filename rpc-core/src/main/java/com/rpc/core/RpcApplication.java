abbf5da79bfcc2ebc1fa8a91f54b83d3296bb8ea
Hangz <1506620085@qq.com>
2025-07-09 17:26:28 +0800

    支持配置和扩展注册中心

--- rpc-core/src/main/java/com/rpc/core/RpcApplication.java
+++ rpc-core/src/main/java/com/rpc/core/RpcApplication.java
@@ -1,7 +1,10 @@
 package com.rpc.core;
 
+import com.rpc.core.config.RegistryConfig;
 import com.rpc.core.config.RpcConfig;
 import com.rpc.core.constant.RpcConstant;
+import com.rpc.core.registry.Registry;
+import com.rpc.core.registry.RegistryFactory;
 import com.rpc.core.utils.ConfigUtils;
 import lombok.extern.slf4j.Slf4j;
 
@@ -17,6 +20,11 @@ public class RpcApplication {
     public static void init(RpcConfig newRpcConfig) {
         rpcConfig = newRpcConfig;
         log.info("rpc init, config = {}", newRpcConfig.toString());
+        // 注册中心初始化
+        RegistryConfig registryConfig = rpcConfig.getRegistryConfig();
+        Registry registry = RegistryFactory.getInstance(registryConfig.getRegistry());
+        registry.init(registryConfig);
+        log.info("registry init, config = {}", registryConfig);
     }
 
     public static void init() {
