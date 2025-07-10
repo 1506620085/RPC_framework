550124b427145d583081cb37f4ae3d41234157aa
Hangz <1506620085@qq.com>
2025-07-10 11:21:10 +0800

    完善调用流程

--- rpc-core/src/main/java/com/rpc/core/proxy/ServiceProxy.java
+++ rpc-core/src/main/java/com/rpc/core/proxy/ServiceProxy.java
@@ -1,15 +1,22 @@
 package com.rpc.core.proxy;
 
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
 import com.rpc.core.RpcApplication;
+import com.rpc.core.config.RpcConfig;
+import com.rpc.core.constant.RpcConstant;
 import com.rpc.core.model.RpcRequest;
 import com.rpc.core.model.RpcResponse;
+import com.rpc.core.model.ServiceMetaInfo;
+import com.rpc.core.registry.Registry;
+import com.rpc.core.registry.RegistryFactory;
 import com.rpc.core.serializer.Serializer;
 import com.rpc.core.serializer.SerializerFactory;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.util.List;
 
 /**
  * 动态代理
@@ -27,8 +34,9 @@ public class ServiceProxy implements InvocationHandler {
         // 指定序列化器
         Serializer serializer = SerializerFactory.getInstance(RpcApplication.getRpcConfig().getSerializer());
         // 构造请求
+        String serviceName = method.getDeclaringClass().getName();
         RpcRequest rpcRequest = RpcRequest.builder()
-                .serviceName(method.getDeclaringClass().getName())
+                .serviceName(serviceName)
                 .methodName(method.getName())
                 .parameterTypes(method.getParameterTypes())
                 .args(args)
@@ -36,14 +44,26 @@ public class ServiceProxy implements InvocationHandler {
         try {
             // 序列化
             byte[] bodyBytes = serializer.serialize(rpcRequest);
-
+            // 从注册中心获取服务提供者请求地址
+            RpcConfig rpcConfig = RpcApplication.getRpcConfig();
+            Registry registry = RegistryFactory.getInstance(rpcConfig.getRegistryConfig().getRegistry());
+            ServiceMetaInfo serviceMetaInfo = new ServiceMetaInfo();
+            serviceMetaInfo.setServiceName(serviceName);
+            serviceMetaInfo.setServiceVersion(RpcConstant.DEFAULT_SERVICE_VERSION);
+            List<ServiceMetaInfo> serviceMetaInfoList = registry.serviceDiscovery(serviceMetaInfo.getServiceKey());
+            if (CollUtil.isEmpty(serviceMetaInfoList)) {
+                throw new RuntimeException("暂无服务地址");
+            }
+            // 暂时先取第一个
+            ServiceMetaInfo selectedServiceMetaInfo = serviceMetaInfoList.get(0);
             // 发送请求
             byte[] result;
-            try (HttpResponse httpResponse = HttpRequest.post("http://localhost:8080")
+            try (HttpResponse httpResponse = HttpRequest.post(selectedServiceMetaInfo.getServiceAddress())
                     .body(bodyBytes)
                     .execute()) {
                 result = httpResponse.bodyBytes();
             }
+            // 反序列化
             RpcResponse deserialize = serializer.deserialize(result, RpcResponse.class);
             return deserialize.getData();
         } catch (Exception e) {
