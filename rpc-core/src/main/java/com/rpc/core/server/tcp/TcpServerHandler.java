202a58b326199837f5772f0dba4824f487a941a9
Hangz <1506620085@qq.com>
2025-07-24 17:49:27 +0800

    TCP 请求处理器（服务提供者）

---
+++ rpc-core/src/main/java/com/rpc/core/server/tcp/TcpServerHandler.java
@@ -0,0 +1,65 @@
+package com.rpc.core.server.tcp;
+
+import com.rpc.core.model.RpcRequest;
+import com.rpc.core.model.RpcResponse;
+import com.rpc.core.protocol.ProtocolMessage;
+import com.rpc.core.protocol.ProtocolMessageDecoder;
+import com.rpc.core.protocol.ProtocolMessageEncoder;
+import com.rpc.core.protocol.ProtocolMessageTypeEnum;
+import com.rpc.core.registry.LocalRegistry;
+import io.vertx.core.Handler;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.net.NetSocket;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+
+/**
+ * TCP 请求处理器（服务提供者）
+ */
+public class TcpServerHandler implements Handler<NetSocket> {
+
+
+    @Override
+    public void handle(NetSocket netSocket) {
+        // --处理连接--
+        netSocket.handler(buffer -> {
+            // 接受请求，解码
+            ProtocolMessage<RpcRequest> protocolMessage;
+            try {
+                protocolMessage = (ProtocolMessage<RpcRequest>) ProtocolMessageDecoder.decode(buffer);
+            } catch (IOException e) {
+                throw new RuntimeException("协议消息解码错误");
+            }
+            RpcRequest rpcRequest = protocolMessage.getBody();
+
+            // --处理请求--
+            RpcResponse rpcResponse = new RpcResponse();
+            try {
+                // 获取要调用的服务实现类，通过反射调用
+                Class<?> implClass = LocalRegistry.get(rpcRequest.getServiceName());
+                Method method = implClass.getMethod(rpcRequest.getMethodName(), rpcRequest.getParameterTypes());
+                Object result = method.invoke(implClass.newInstance(), rpcRequest.getArgs());
+                // 封装返回结果
+                rpcResponse.setData(result);
+                rpcResponse.setDataType(method.getReturnType());
+                rpcResponse.setMessage("ok");
+            } catch (Exception e) {
+                e.printStackTrace();
+                rpcResponse.setMessage(e.getMessage());
+                rpcResponse.setException(e);
+            }
+            // --发送响应，编码--
+            ProtocolMessage.Header header = protocolMessage.getHeader();
+            header.setType((byte) ProtocolMessageTypeEnum.RESPONSE.getKey());
+            ProtocolMessage<RpcResponse> responseProtocolMessage = new ProtocolMessage<>(header, rpcResponse);
+
+            try {
+                Buffer encode = ProtocolMessageEncoder.encode(responseProtocolMessage);
+                netSocket.write(encode);
+            } catch (IOException e) {
+                throw new RuntimeException("协议消息编码错误");
+            }
+        });
+    }
+}
