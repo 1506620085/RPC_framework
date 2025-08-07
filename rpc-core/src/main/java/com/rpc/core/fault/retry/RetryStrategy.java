aadd0015f95beaa65aa74de5c1eac1375be0ff59
Hangz <1506620085@qq.com>
2025-08-07 16:24:28 +0800

    多种策略实现

---
+++ rpc-core/src/main/java/com/rpc/core/fault/retry/RetryStrategy.java
@@ -0,0 +1,21 @@
+package com.rpc.core.fault.retry;
+
+import com.rpc.core.model.RpcResponse;
+
+import java.util.concurrent.Callable;
+
+/**
+ * 重试策略
+ */
+public interface RetryStrategy {
+
+
+    /**
+     * 重试
+     *
+     * @param callable
+     * @return
+     * @throws Exception
+     */
+    RpcResponse doRetry(Callable<RpcResponse> callable) throws Exception;
+}
