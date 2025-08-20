16190baf62a81438b1c758bf86d241b2111d28b4
Hangz <1506620085@qq.com>
2025-08-20 16:55:41 +0800

    定义注解

---
+++ rpc-spring-boot-starter/src/main/java/com/rpc/springboot/starter/annotation/EnableRpcProvider.java
@@ -0,0 +1,21 @@
+package com.rpc.springboot.starter.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 启用提供者 RPC 注解
+ */
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface EnableRpcProvider {
+
+    /**
+     * 需要启动提供者 server
+     *
+     * @return
+     */
+    boolean needServer() default true;
+}
---
+++ rpc-spring-boot-starter/src/test/java/com/rpc/springboot/starter/RpcSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpc.springboot.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpcSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
